6945947fa1f36d5616c050fb53060f875639b196
Brian Miles <selimnairb@gmail.com>
2015-05-14 14:34:00 -0400

    2.0.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='rhessyscalibrator',
-      version='2.0.0-dev',
+      version='2.0.0',
       description='Libraries and command-line scripts for handling RHESSys model calibration.',
       long_description=readme(),
       classifiers=[
